bf084eabc7d51bf65a0d282edcd226d19db70477
johancberg <johancberg@outlook.com>
2023-07-31 00:47:03 +0200

    Install File Transfer

--- src-cordova/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ src-cordova/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -223,6 +223,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports.metadata = {
     "cordova-plugin-geolocation": "4.0.2",
     "cordova-plugin-splashscreen": "6.0.0",
+    "cordova-plugin-whitelist": "1.3.4",
     "cordova-plugin-file": "8.0.0",
     "cordova-plugin-file-transfer": "2.0.0-dev"
   };
