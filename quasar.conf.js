fc350653233fb0f687e50814c73ac7b3c34d6b28
johancberg <johancberg@outlook.com>
2023-08-08 19:03:17 +0200

    Raise the android version to 2.2.4

--- quasar.conf.js
+++ quasar.conf.js
@@ -152,7 +152,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
     cordova: {
       // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
-      version: '2.2.3'
+      version: '2.2.4'
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
